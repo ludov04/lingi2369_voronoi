17c58c8c164b4f4c1b7caa8906b78b50bbe2b08f
Ludovic Vannoorenberghe <ludovic.v@me.com>
2015-04-24 11:17:33 +0200

    geoscript lib

---
+++ src/common/Model.scala
@@ -0,0 +1,7 @@
+package common
+
+/**
+ * Created by ludov on 24/04/15.
+ */
+
+case class Point(x: Int, y: Int)
---
+++ src/common/model/Point.scala
@@ -0,0 +1,6 @@
+package common.model
+
+/**
+ * Created by ludov on 24/04/15.
+ */
+case class Point(x: Int, y: Int)
