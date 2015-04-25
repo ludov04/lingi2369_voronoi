3812cd73ba5cb1a313408dfe6cc86a6f5547cbe2
Aruk <sebastien.f.tack@gmail.com>
2015-04-25 21:44:23 +0200

    Starting of the GUI (without JST testbuilder :( )

---
+++ src/main/scala/Drawer.scala
@@ -0,0 +1,34 @@
+/**
+ * Created by Fabian on 25-04-15.
+ */
+
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Graphics
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import java.util.LinkedList
+
+import javax.swing.JButton
+import javax.swing.JComponent
+import javax.swing.JFrame
+import javax.swing.JPanel
+
+import com.vividsolutions.jts.geom.GeometryCollection
+
+class Drawer extends JComponent {
+  var content = GeometryCollection
+
+  def Drawer (content : GeometryCollection): Unit ={
+    this.content = content
+  }
+
+  override def paintComponent(g: Graphics) {
+    super.paintComponent(g)
+    for(i <- 0 until content.getNumGeometries){
+
+    }
+  }
+
+}
---
+++ src/main/scala/gui.scala
@@ -0,0 +1,28 @@
+/**
+ * Created by Fabian on 25-04-15.
+ */
+
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Graphics
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import java.util.LinkedList
+
+import javax.swing.JButton
+import javax.swing.JComponent
+import javax.swing.JFrame
+import javax.swing.JPanel
+
+class Gui {
+
+  def main(args: Array[String]) {
+    val frame = new JFrame()
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+
+
+
+  }
+
+}
