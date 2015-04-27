379b610e315f74a1a5406c02fc6909afb2cb97e4
Ludovic Vannoorenberghe <ludovic.v@me.com>
2015-04-27 16:56:26 +0200

    Red-Black Tree

--- src/main/scala/Fortune.scala
+++ src/main/scala/Fortune.scala
@@ -1,7 +1,8 @@
 import com.vividsolutions.jts.geom.{Coordinate, Point}
 
 import scala.collection.mutable
-
+import scalacaster.Tree
+import scalacaster.Tree._
 /**
  * Created by ludov on 27/04/15.
  */
@@ -19,11 +20,15 @@ class Fortune {
 
   val q = new mutable.PriorityQueue[Event]()
   val edgeList : DCEL = new DCEL()
-  val tree = new mutable.TreeSet[Arc]()
+  val tree = Tree.empty[Arc]
 
   def handleCircleEvent(a : Arc) = ???
 
-  def handleSiteEvent(p: Coordinate) = ???
+  def handleSiteEvent(p: Coordinate) = {
+    if (tree.isEmpty) {
+      tree.add(new Arc(p))
+    }
+  }
 
   def checkCircleEvent(a : Arc) = ???
 
